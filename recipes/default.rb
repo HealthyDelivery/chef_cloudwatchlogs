4f1ef35ad3fb690e87307d6147b7c26b86b15f44
Daniel Nelson <daniel.wolcott.nelson@gmail.com>
2015-02-06 21:45:56 -0800

    info

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,9 @@
 # All rights reserved - Do Not Redistribute
 #
 
+Chef::Log.info('node.to_json')
+Chef::Log.info(JSON.parse(node.to_json))
+
 rails_log_dir = '/srv/www/platejoy/current/log/'
 rails_env = node[:environment][:framework_env]
 
